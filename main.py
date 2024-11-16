20cfe08cc23e75c154429181ce70a79749b629f3
Max Kalhama <kalhama.max@gmail.com>
2024-11-16 12:55:28 +0200

    style: format

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-import requests 
-from bs4 import BeautifulSoup 
+import requests
+from bs4 import BeautifulSoup
 from time import sleep
 import math
 import urllib
@@ -8,10 +8,12 @@ import pandas as pd
 from typing import Optional
 
 df: Optional[pd.DataFrame] = None
-CSV_PATH = 'file.csv'
+CSV_PATH = "file.csv"
+
 
 def snake_case(str):
-    return str.strip().replace(" ", "_").lower().replace('-','_')        
+    return str.strip().replace(" ", "_").lower().replace("-", "_")
+
 
 default_headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:130.0) Gecko/20100101 Firefox/130.0",
@@ -21,14 +23,11 @@ default_headers = {
     "Sec-Fetch-Dest": "empty",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
-    "Priority": "u=0"
+    "Priority": "u=0",
 }
 
-auth_headers = {
-    'OTA-token': None,
-    'OTA-loaded': None,
-    'OTA-cuid': None
-}
+auth_headers = {"OTA-token": None, "OTA-loaded": None, "OTA-cuid": None}
+
 
 def get_wrapper(url, with_authentication):
     headers = {**default_headers}
@@ -36,28 +35,30 @@ def get_wrapper(url, with_authentication):
     if with_authentication:
         headers.update(auth_headers)
 
-    r = requests.get(url,  headers=headers)
+    r = requests.get(url, headers=headers)
 
     r.raise_for_status()
-    
+
     return r
 
 
 def authenticate():
     print("authenticating")
-    r = get_wrapper('https://asunnot.oikotie.fi', False)
+    r = get_wrapper("https://asunnot.oikotie.fi", False)
 
-    soup = BeautifulSoup(r.content, 'html.parser')
+    soup = BeautifulSoup(r.content, "html.parser")
 
-    auth_headers['OTA-loaded'] = soup.find('meta', {'name': 'loaded'})['content']
-    auth_headers['OTA-token'] = soup.find('meta', {'name': 'api-token'})['content']
-    auth_headers['OTA-cuid'] = soup.find('meta', {'name': 'cuid'})['content']
+    auth_headers["OTA-loaded"] = soup.find("meta", {"name": "loaded"})["content"]
+    auth_headers["OTA-token"] = soup.find("meta", {"name": "api-token"})["content"]
+    auth_headers["OTA-cuid"] = soup.find("meta", {"name": "cuid"})["content"]
 
 
 def get_retry(url, with_authentication):
     retries = 5
     for i in range(1, retries):
-        if (i >= 2 and with_authentication) or (retries == 1 and with_authentication and auth_headers['OTA-token'] == None):
+        if (i >= 2 and with_authentication) or (
+            retries == 1 and with_authentication and auth_headers["OTA-token"] == None
+        ):
             authenticate()
 
         try:
@@ -76,39 +77,47 @@ def get_retry(url, with_authentication):
 
             if i == retries:
                 return None
-            
+
             sleep(pow(i, 2))
 
 
 def get_cards(page):
     offset = (page - 1) * 24
 
-    json = get_retry('https://asunnot.oikotie.fi/api/search?cardType=100&limit=24&locations=[[64,6,"Helsinki"],[39,6,"Espoo"],[65,6,"Vantaa"],[130,6,"Kauniainen"]]&offset=' + str(offset) + '&sortBy=published_sort_desc', True).json()
+    json = get_retry(
+        'https://asunnot.oikotie.fi/api/search?cardType=100&limit=24&locations=[[64,6,"Helsinki"],[39,6,"Espoo"],[65,6,"Vantaa"],[130,6,"Kauniainen"]]&offset='
+        + str(offset)
+        + "&sortBy=published_sort_desc",
+        True,
+    ).json()
 
     return json
 
+
 def get_listing(id):
-    if df is None or df.empty or 'id' not in df.columns:
+    if df is None or df.empty or "id" not in df.columns:
         return None
 
-    row = df[df['id'] == id]
+    row = df[df["id"] == id]
     return row if not row.empty else None
 
 
-def save_listing(card):    
+def save_listing(card):
     data = {
-        'address': card['location']['address'],
-        'district': card['location']['district'],
-        'city': card['location']['city'],
-        'zip_code': card['location']['zipCode'],
-        'country': card['location']['country'],
-        'latitude': card['location']['latitude'],
-        'longitude': card['location']['longitude'],
-        'published': card['meta']['published'],
-        'id': card['cardId'],
+        "address": card["location"]["address"],
+        "district": card["location"]["district"],
+        "city": card["location"]["city"],
+        "zip_code": card["location"]["zipCode"],
+        "country": card["location"]["country"],
+        "latitude": card["location"]["latitude"],
+        "longitude": card["location"]["longitude"],
+        "published": card["meta"]["published"],
+        "id": card["cardId"],
     }
 
-    r = get_retry('https://asunnot.oikotie.fi/myytavat-asunnot/espoo/' + str(data['id']), True)
+    r = get_retry(
+        "https://asunnot.oikotie.fi/myytavat-asunnot/espoo/" + str(data["id"]), True
+    )
     # very occasionally, search results do not exxist. Just skip them...
     if r is None:
         print("Nothing found, skipping...")
@@ -116,29 +125,30 @@ def save_listing(card):
 
     html = r.content
 
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
 
-    rows = soup.find_all(class_='info-table__row')
+    rows = soup.find_all(class_="info-table__row")
 
     for row in rows:
-        title = row.find(class_='info-table__title')
-        value = row.find(class_='info-table__value')
+        title = row.find(class_="info-table__title")
+        value = row.find(class_="info-table__value")
         if title and value:
             data[snake_case(title.text)] = value.text
 
-    description_div = soup.find('div', class_='listing-overview')
-    
+    description_div = soup.find("div", class_="listing-overview")
+
     if description_div:
-        paragraphs = description_div.find_all('p')
-        data["description"] = '\n\n'.join(p.get_text(strip=True) for p in paragraphs)
+        paragraphs = description_div.find_all("p")
+        data["description"] = "\n\n".join(p.get_text(strip=True) for p in paragraphs)
     else:
         print("No description??")
-        data['description'] = ''
-    
+        data["description"] = ""
+
     global df
 
     df = pd.concat([df, pd.DataFrame([data])], ignore_index=True)
     df.to_csv("file.csv", index=False)
+
 
 def initialize_df():
     global df
@@ -150,7 +160,8 @@ def initialize_df():
     except Exception as e:
         df = pd.DataFrame()
 
-def main():    
+
+def main():
     authenticate()
 
     page, total_pages = (1, 1)
@@ -161,9 +172,9 @@ def main():
         print(f"Processing page {page} of {total_pages}")
         resp = get_cards(page)
         total_pages = math.ceil(resp["found"] / 24)
-        
+
         for card in resp["cards"]:
-            listing = get_listing(card['cardId'])
+            listing = get_listing(card["cardId"])
             if listing is None:
                 print(f"Saving {card['cardId']}. Shape: {df.shape}")
                 listing = save_listing(card)
